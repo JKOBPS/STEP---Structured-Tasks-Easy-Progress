51f817ead4184407cebcd9b756d58c0c089e94d6
JKOBPS <jacobparrasilva@gmail.com>
2026-07-24 21:23:36 +0200

    Vercel.json added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <Routes>
       {/*RUTAS PÚBLICAS (Sin restricciones)*/}
       <Route path="/" element={<Navigate to="/landing" replace />} />
+      <Route path="/landing" element={<Navigate to="/landing" replace />} />
       <Route path="/landing" element={<LandingPage />} />
 
       {/*RUTAS PRIVADAS (Protegidas)*/}
